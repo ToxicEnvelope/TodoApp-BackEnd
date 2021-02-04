fd1856644298a8d6651a8f9b0cd951ed0af0696a
ToxicEnvelope <ynhoffman@gmail.com>
2021-02-04 15:57:31 +0200

    add db-path-support for Windows

--- backend/database.py
+++ backend/database.py
@@ -2,10 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from os.path import dirname, abspath
+from platform import platform
+
+isWindows = lambda: platform().lower().startswith('win')
 
 current_dir = dirname(dirname(abspath(__file__)))
 
-engine = create_engine(f'sqlite:////{current_dir}/todo-tasks.db', convert_unicode=True)
+db_path_string = f'sqlite:////{current_dir}/todo-task.db' if not isWindows() else r'sqlite:///{0}\todo-tasks.db'.format(current_dir)
+
+engine = create_engine(db_path_string, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
