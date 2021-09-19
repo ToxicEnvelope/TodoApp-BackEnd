9a89cdb7c141f2cdf0a96db3423148e37eb6d2b3
Yahav-N-Hoffman <yahavh@avanan.com>
2021-09-19 20:28:36 +0300

    simplifying + add try except block

--- backend/database.py
+++ backend/database.py
@@ -1,15 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from os.path import dirname, abspath
-from platform import platform
+from backend.helpers import PathUtil
 
-isWindows = lambda: platform().lower().startswith('win')
 
-current_dir = dirname(dirname(abspath(__file__)))
-
-db_path_string = f'sqlite:////{current_dir}/todo-task.db' if not isWindows() else r'sqlite:///{0}\todo-tasks.db'.format(current_dir)
-
+db_path_string = PathUtil.get_database_file()
 engine = create_engine(db_path_string, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
@@ -17,6 +12,9 @@ db_session = scoped_session(sessionmaker(autocommit=False,
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-
 def db_init():
-    Base.metadata.create_all(bind=engine)
+    try:
+        Base.metadata.create_all(bind=engine)
+    except:
+        return False
+    return True
