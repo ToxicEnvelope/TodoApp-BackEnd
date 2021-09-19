520e02a9cb362e18dcb77319874d8cb7a00d6245
Yahav-N-Hoffman <yahavh@avanan.com>
2021-09-20 00:34:17 +0300

    refactors

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,32 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from os.path import dirname, abspath
+from os.path import dirname, abspath, join
 from platform import platform
+import sys
 
-isWindows = lambda: platform().lower().startswith('win')
+global name
 
-current_dir = dirname(dirname(abspath(__file__)))
+def path_resolver():
+    global name
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        base_path = getattr(sys, '_MEIPASS', dirname(abspath(__file__)))
+    else:
+        base_path = join(dirname(abspath(__file__)), '..')
+    if platform().lower().startswith('win'):
+        final_dir = r'%s_%s' % (base_path, name)
+        relative_path = r'sqlite:///%s\todo-app.db' % final_dir
+    else:
+        final_dir = '%s_%s' % (base_path, name)
+        relative_path = 'sqlite:////%s/todo-app.db' % final_dir
 
-db_path_string = f'sqlite:////{current_dir}/todo-task.db' if not isWindows() else r'sqlite:///{0}\todo-tasks.db'.format(current_dir)
+    final_path = join(final_dir, relative_path)
+    sys.stdout.write(' [+] Database Location Path : %s' % final_path)
+    return final_path
+
+db_path_string = path_resolver()
+"""db_path_string = f'sqlite:////{current_dir}/todo-task.db' if not isWindows() else r'sqlite:///{0}\todo-tasks.db'.format(current_dir)"""
 
 engine = create_engine(db_path_string, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
--- todo_restful_app.py
+++ todo_restful_app.py
@@ -2,5 +2,8 @@ from backend.app import app
 
 
 if __name__ == '__main__':
+    global name
+    import sys
+    name = sys.argv[1]
     app.run(host='0.0.0.0', port=9000)
 
