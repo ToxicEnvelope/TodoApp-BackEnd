db35651b8004ec39731aee69c7e975dc7a4109a9
Yahav-N-Hoffman <yahavh@avanan.com>
2021-09-19 22:42:35 +0300

    refactors

--- todo_restful_app.py
+++ todo_restful_app.py
@@ -1,8 +1,6 @@
-import sys
 from backend.app import app
 
 
 if __name__ == '__main__':
-    args = sys.argv
-    app.run(host='0.0.0.0', port=8081, debug=True if '--debug' in args else False)
+    app.run(host='0.0.0.0', port=9000)
 
